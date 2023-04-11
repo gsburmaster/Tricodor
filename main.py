964ab7996b5fd5de00b53a752ecbe0fcad3a1eea
Gage B <gsburmaster@ku.edu>
2023-04-11 16:32:37 -0500

    found the link in the docs that is useful

---
+++ main.py
@@ -0,0 +1,3 @@
+import circuitgraph as cg
+circuit = cg.parsing.verilog.parse_verilog_netlist()
+#https://circuitgraph.github.io/circuitgraph/parsing/verilog.html#circuitgraph.parsing.verilog.parse_verilog_netlist
