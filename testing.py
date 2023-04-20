5b414ecd8686d2351538f0a75d8b455bc16151e3
Gage B <gsburmaster@ku.edu>
2023-04-20 11:33:55 -0500

    code that does not work

--- testing.py
+++ testing.py
@@ -5,35 +5,33 @@ c = cg.from_file('benchmarks/s27.v', blackboxes=dff)
 print(c.nodes())
 def isDFFin(input):
     if ".D" in input:
+        # print(input)
         return True
     return False
 def isDFFout(input):
     if ".Q" in input:
         return True
     return False
-DFFinList = filter(isDFFin,c.nodes())
-DFFoutList = filter(isDFFout,c.nodes())
+DFFinList = list(filter(isDFFin,c.nodes()))
+DFFoutList = list(filter(isDFFout,c.nodes()))
 #https://www.geeksforgeeks.org/breadth-first-search-or-bfs-for-a-graph/#
 def closestDFFin(node, DFFinList,circuit,distance):
-    print('about to print dfflist')
-    for x in DFFinList:
-        print(x)
-    distance = -1
+    # print('about to print dfflist')
+    distance = 0
     for dff in DFFinList:
         paths = circuit.paths(node,dff)
         for path in paths:
-            print(node,path)
-            if len(path) < distance or distance == -1:
+            # print(node,path)
+            if len(path) < distance or distance == 0:
                 distance = len(path)
-        print(node,distance)
+    # print(node,distance)
     return distance
-        
 
 
-
-
+print(cg.props.avg_sensitivity(cg.tx.strip_blackboxes(c),"G5"))
 
 for node in c.nodes():
+#    print(cg.props.sensitivity(cg.tx.strip_blackboxes(c),node))
    closestDFFin(node,DFFinList,c,0)
 #gonna do BFS to find nearest 
 
