d979630f7b2a0d3873d81b404b771e0b4ace828c
gsburmaster <gsburmaster@gmail.com>
2023-04-25 15:24:04 -0500

    pushing to work on laptop
    adding closest input distance

--- testing.py
+++ testing.py
@@ -1,10 +1,24 @@
 import circuitgraph as cg
-import pdb
 dff = [cg.BlackBox("dff", ["CK", "D"], ["Q"])]
 c = cg.from_file('benchmarks/s27.v', blackboxes=dff)
 print(c.nodes())
+
+# this takes one array of all dff ports and sorts into multiple arrays of each dff (so an array of DFF_1, an array of DFF_2, etc)
+def sortDFFs(dfflistin):
+    dfflist = [] + dfflistin
+    outputList = []
+    if (len(dfflist) == 0):
+        return outputList
+    while (len(dfflist) > 0):
+        sortFilter = dfflist[0].replace('.D','')
+        sortFilter = sortFilter.replace('.Q','')
+        sortFilter = sortFilter.replace('.CK','')
+        outputList+=[[k for k in dfflist if sortFilter in k]]
+        dfflist = list(set(dfflist) - set(list(filter(lambda k: sortFilter in k,dfflist))))
+    return outputList
+
 def isDFFatall(input):
-    if ".D" in input or '.Q' or '.CK' in input:
+    if ".D" in input or '.Q' in input or '.CK' in input:
         return True
     return False
 def isDFFin(input):
@@ -86,10 +100,30 @@ def closestDFFout(node,DFFinList,circuit):
     # print(node,distance)
     return distance
 
+
+def closestInput(node,circuit,dfflist):
+    dffsortedlist = sortDFFs(dfflist)
+    inputs = list(circuit.inputs())
+    distance = -1
+    for input in inputs:
+        paths = list(circuit.paths(input,node))
+        if (len(paths) > 0):
+            path = min(paths)
+            if (distance == -1) or (len(path) -1 < distance):
+                distance = len(path)-1
+        for dffArr in dffsortedlist:
+            #need to check distance of each path- one dff could be shorter to, but longer after vs another that is medium on both.
+            doubleLoopTmp = -1
+            for dffPort in dffArr:
+                fastestPath = min(list(circuit.paths(node,dffPort)))
+                
+
+            
+sortDFFs(DFFList)
 # print(cg.props.avg_sensitivity(cg.tx.strip_blackboxes(c),"G5"))
 # print('paths for output',list(c.paths("G11","DFF_1.D")))
 for node in c.nodes():
    closestDFFin(node,DFFList,c)
-   closestDFFout(node,DFFList,c)
-#gonna do BFS to find nearest 
+#    closestDFFout(node,DFFList,c)
+#    closestInput(node,c,DFFList)
 
